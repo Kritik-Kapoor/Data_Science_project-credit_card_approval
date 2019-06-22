81917194c85a093cd8e6e8ec4245d881ea1ccc92
montubhaiya <51992163+montubhaiya@users.noreply.github.com>
2019-06-22 10:38:22 +0530

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=10
+if(x<15):
+    print("the no. is less than 15")
+    
+
+
+# In[4]:
+
+
+x=10
+if(x>15):
+    print("halo")
+else:
+    print("bye")
+
+
+# In[6]:
+
+
+x=10
+y=20
+z=15
+if(x>y and y>z):
+    print("x is greater")
+if(y>x and y>z):
+    print("y is greater")
+else :
+    print("z is greater")
+
+
+# In[9]:
+
+
+x=10
+y=10
+if(x==y):
+    print(x*x)
+else:
+    print(x*y)
+
+
+# In[11]:
+
+
+x=10
+if(x==0):
+    print("zero")
+elif(x>0):
+    print("positive")
+else:
+    print("negative")
+
+
+# In[12]:
+
+
+n=1
+while(n<10):
+    print(n)
+    n=n+1
+
+
+# In[ ]:
+
+
+
+
